c831575acc081d67b6f17b4a8206e6423633e70c
jxm6165 <35394963+jxm6165@users.noreply.github.com>
2020-03-29 21:17:08 -0400

    Create Block_Extract_Pass.cpp

---
+++ Code_Snippet_Extarction/Block_Extract_Pass.cpp
@@ -0,0 +1,33 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+using namespace llvm;
+
+namespace {
+  struct SkeletonPass : public FunctionPass {
+    static char ID;
+    SkeletonPass() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F) {
+      for (auto& B : F) {
+        errs() << "Basic block:\n";
+        B.dump();
+      }
+      return false;
+    }
+  };
+}
+
+char SkeletonPass::ID = 0;
+
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
+static void registerSkeletonPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM) {
+  PM.add(new SkeletonPass());
+}
+static RegisterStandardPasses
+  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                 registerSkeletonPass);
